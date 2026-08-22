99dafc7fcf7a8444fb1c7bed293b7deb8182ccf7
Dessiree Camille H. Pasion <dessireecamillepasion65@gmail.com>
2026-08-22 17:26:15 +0800

    remove: one repository "Qualifiying-Project--website-"

--- src/services/github.ts
+++ src/services/github.ts
@@ -35,7 +35,7 @@ export async function fetchGithubRepositories(username: string) {
 
     const repositories = (await response.json()) as GithubRepository[]
 
-    const excludedRepositories = ['truehue-website', 'siridc', 'ob-gyn-clinic_copy']
+    const excludedRepositories = ['truehue-website', 'siridc', 'ob-gyn-clinic_copy', 'Qualifiying-Project--website-']
 
     const filteredRepositories = repositories
       .filter(
